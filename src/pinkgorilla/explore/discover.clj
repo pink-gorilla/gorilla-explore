0dd4b436c8183f9a5b55fd3045cc314ccd874fc1
Andreas Steffan <a.steffan@contentreich.de>
2020-01-21 17:18:39 +0100

    "Broken" exclusion introduced

--- src/pinkgorilla/explore/discover.clj
+++ src/pinkgorilla/explore/discover.clj
@@ -41,6 +41,7 @@
     (= (:repo storage) "notebook-encoding") true
     (= (:filename storage) "meta1.cljg") true
     (= (:filename storage) "unittest-meta1.cljg") true
+    (and (:filename storage) (.contains (:filename storage) "broken/")) true
     :else false))
 
 (defn remove-excluded [storages]
