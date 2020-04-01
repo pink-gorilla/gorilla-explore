42b3f9555a61f7694c32d3f1b945ba7a82fba5c4
CI/CD <ci@pinkgorilla.org>
2020-04-01 05:43:39 +0000

    Release 0.1.18

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-explore "0.1.18-SNAPSHOT"
+(defproject org.pinkgorilla/gorilla-explore "0.1.18"
   :description "Explore PinkGorilla notebooks (private and public) on github."
   :url "https://github.com/pink-gorilla/gorilla-explore"
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
