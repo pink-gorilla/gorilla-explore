cbbdff3b44c72e1054ca6c88f37c28244b8c48c6
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-06-27 19:00:52 -0500

    deps fixed

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
   :source-paths ["src/clj"
                  "src/cljc"
                  "src/cljs" ; cljshas to go into jar too.
-                 ] ; "test"
+] ; "test"
   ;:test-paths ["test"]
   :resource-paths  ["resources" ; not from npm
                     #_"target/node_modules"] ; css png resources from npm modules
@@ -27,13 +27,12 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "1.1.582"]
                  [org.clojure/tools.cli "1.0.194"]
-                 [com.taoensso/timbre "4.10.0"] ; cljs logging
-
+                 [com.taoensso/timbre "4.10.0"] ; clj/cljs logging
                  [clojure.java-time "0.3.2"]
                   ; dependencies used for discovery:
                  [irresponsible/tentacles "0.6.6"] ; github api  ; https://github.com/clj-commons/tentacles
                  [com.cemerick/url "0.1.1"]  ; url query-strings
-                 [clj-http "3.10.0"]  ; http requests
+                 [clj-http "3.10.1"]  ; http requests
                  [cheshire "5.10.0"]  ; JSON parsings
                  [throttler "1.0.0" ; api rate-limits ; has very old core.async
                   :exclusions  [[org.clojure/clojure]
@@ -52,6 +51,15 @@
                  [ring/ring-core "1.8.1"]
                  [ring/ring-defaults "0.3.2"
                   :exclusions [javax.servlet/servlet-api]]
+                 ; frontend
+                 [reagent "0.10.0"
+                  :exclusions [org.clojure/tools.reader
+                               cljsjs/react
+                               cljsjs/react-dom]]
+                 [re-frame "0.10.9"]
+                 [cljs-ajax "0.8.0"] ; needed for re-frame/http-fx
+                 [day8.re-frame/http-fx "0.1.6"] ; reframe based http requests
+                 [re-com "2.8.0"]      ; reagent reuseable ui components
                  ; pinkgorilla
                  [org.pinkgorilla/notebook-encoding "0.1.2"] ; notebook encoding
                  ]
@@ -69,25 +77,12 @@
                                     [thheller/shadow-cljs "2.8.81"]
                                     [thheller/shadow-cljsjs "0.0.21"]
                                     [org.clojure/clojurescript "1.10.773"]
-                                    [reagent "0.10.0"
-                                     :exclusions [org.clojure/tools.reader
-                                                  cljsjs/react
-                                                  cljsjs/react-dom]]
-                                    [re-frame "0.10.9"]
-                                    [cljs-ajax "0.8.0"] ; needed for re-frame/http-fx
-                                    [day8.re-frame/http-fx "0.1.6"] ; reframe based http requests
-
-
-
                                     ;[ring-middleware-format "0.7.4"] ; replaced by muuntaja
-                                    [ring/ring-json "0.5.0"]
-                                    [bk/ring-gzip "0.3.0"] ; from oz
-                                    [clj-http "3.10.1"]
-                                    [re-com "2.8.0"]      ; reagent reuseable ui components
+                                    ;[ring/ring-json "0.5.0"]
+                                    ;[bk/ring-gzip "0.3.0"] ; from oz
                                     ]}
 
-
-             :dev {:source-paths ["profiles/dev/src" 
+             :dev {:source-paths ["profiles/dev/src"
                                   "test"]
                    :dependencies [[clj-kondo "2020.03.20"]]
                    :plugins      [[lein-cljfmt "0.6.6"]
